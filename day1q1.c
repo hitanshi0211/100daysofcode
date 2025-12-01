8569a43e6620aecfa0da662e796efbe34457ffed
hitanshi0211 <hitanshipvt@gmail.com>
2025-12-01 21:33:22 +0530

    Add files via upload

---
+++ day1q1.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+
+int main(){
+
+    int a,b,s;
+    printf("enter your first number : \n");
+    scanf("%d",&a);
+
+    printf("enter the second number: \n");
+    scanf("%d",&b);
+
+    s = a + b ;
+
+    printf("sum of the two numbers = %d", s);
+
+
+
+
+    return 0;
+}
